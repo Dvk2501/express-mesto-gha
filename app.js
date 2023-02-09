e1327a5ffe57dd3f1a8eef1f1541f95a7fdab500
Dmitry Kravchenko <dmitry.kravchenko96@yandex.ru>
2023-02-09 13:25:55 +0300

    1.4

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
-const { NOT_FOUND } = require('./constats');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { signUp, signIn } = require('./middlewares/validation');
+const NotFoundError = require('./errors/NotFoundError');
+const handlerErrors = require('./middlewares/handleError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -22,24 +23,10 @@ app.use(auth);
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
-app.use('/*', (req, res) => {
-  res.status(NOT_FOUND).send({ message: 'Страница не найдена' });
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
 });
-
 app.use(errors());
-app.use((err, req, res, next) => {
-  // если у ошибки нет статуса, выставляем 500
-  const { statusCode = 500, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      // проверяем статус и выставляем сообщение в зависимости от него
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-  next();
-});
+app.use(handlerErrors);
 
 app.listen(PORT);

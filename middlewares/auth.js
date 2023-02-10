437752fb1a9e360dd4d736802d2841034c7f6085
Dmitry Kravchenko <dmitry.kravchenko96@yandex.ru>
2023-02-10 17:48:50 +0300

    1,4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,5 @@
 const jwt = require('jsonwebtoken');
-
-const handleAuthError = (res) => {
-  res
-    .status(401)
-    .send({ message: 'Необходима авторизация' });
-};
+const NotAuthError = require('../errors/NotAuthError');
 
 const extractBearerToken = (header) => header.replace('Bearer ', '');
 
@@ -13,7 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res);
+    throw new NotAuthError('Необходима авторизация.');
   }
 
   const token = extractBearerToken(authorization);
@@ -22,10 +17,10 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return handleAuthError(res);
+    next(new NotAuthError('Необходима авторизация'));
   }
 
-  req.user = payload; // записываем пейлоуд в объект запроса
+  req.user = payload;
 
-  next(); // пропускаем запрос дальше
+  next();
 };
